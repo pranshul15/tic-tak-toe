65e6422e1eda3a58efea79466fde3a6306bb5ae2
Pranshul Hatwar <pranshul.hatwar@gmail.com>
2022-05-05 23:33:21 +0530

    5th may last changes

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ console.log("Tic Tac Toe");
 let gameOver = new Audio("gameOver.mp3");
 let music = new Audio("music.mp3");
 let ting = new Audio("ting.mp3");
+let isGameOver = false;
 
 let turn = "X";
 
@@ -10,47 +11,42 @@ const changeTurn = ()=>{
     return (turn==="X")? "O":"X";
 }
 let boxValues = [];
-
+let count = 0;
 // Function to check for a win
 const checkWin = ()=>{
     // horizontal 1
-    if((boxValues[0]==boxValues[1]) && (boxValues[1]==boxValues[2])){
+    if((boxValues[0]===boxValues[1]) && (boxValues[1]===boxValues[2])){
         return boxValues[0];
     }
     // horizontal 2
-    if((boxValues[3]==boxValues[4]) && (boxValues[4]==boxValues[5])){
+    if((boxValues[3]===boxValues[4]) && (boxValues[4]===boxValues[5])){
         return boxValues[3];
     }
     // horizontal 3
-    if((boxValues[6]==boxValues[7]) && (boxValues[7]==boxValues[8])){
+    if((boxValues[6]===boxValues[7]) && (boxValues[7]===boxValues[8])){
         return boxValues[6];
     }
     // vertical 1
-    if((boxValues[0]==boxValues[3]) && (boxValues[3]==boxValues[6])){
+    if((boxValues[0]===boxValues[3]) && (boxValues[3]===boxValues[6])){
         return boxValues[0];
     }
     // vertical 2
-    if((boxValues[1]==boxValues[4]) && (boxValues[4]==boxValues[7])){
+    if((boxValues[1]===boxValues[4]) && (boxValues[4]===boxValues[7])){
         return boxValues[1];
     }
     // vertical 3
-    if((boxValues[2]==boxValues[5]) && (boxValues[5]==boxValues[8])){
+    if((boxValues[2]===boxValues[5]) && (boxValues[5]===boxValues[8])){
         return boxValues[2];
     }
     // diagonal left to right
-    if((boxValues[0]==boxValues[4]) && (boxValues[4]==boxValues[8])){
+    if((boxValues[0]===boxValues[4]) && (boxValues[4]===boxValues[8])){
         return boxValues[0];
     }
     // diagonal right to left
-    if((boxValues[2]==boxValues[4]) && (boxValues[4]==boxValues[6])){
+    if((boxValues[2]===boxValues[4]) && (boxValues[4]===boxValues[6])){
         return boxValues[2];
     }
     return "NO";
-}
-
-const afterWin = (param)=> {
-    document.getElementById('info').innerText = param + " is Winner!";
-    
 }
 
 // Game Logic
@@ -62,10 +58,17 @@ Array.from(boxes).forEach((element) =>{
             boxValues[parseInt(element.id)]=turn; // setting value inside code
             turn = changeTurn(); // changing the turn
             ting.play();
-            if(checkWin()!="NO"){
-                afterWin(checkWin());
+            count++;
+            
+            if(checkWin()==="X" || checkWin==="O"){ 
+                document.getElementById('info').innerText = checkWin() + " is Winner!";
             }
-            document.getElementById('info').innerText = "Turn for " + turn;
+            else if(count == 9){
+                document.getElementById('info').innerText = "Game Draw";
+            }
+            else {
+                document.getElementById('info').innerText = "Turn for " + turn;
+            }
         }
     })
 });
